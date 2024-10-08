0a6029c68633404dcc6aceddad29bfbba1bbae35
Rishita P <pram.73ris@gmail.com>
2024-10-08 12:28:53 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
-import Navbar from './Components/Navbar';
-import Writenote from './Components/write_note';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Writenote from "./Components/write_note";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Writenote />
+      <Navbar />
+      <Writenote />
     </>
   );
 }
